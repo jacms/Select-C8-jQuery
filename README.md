d40619729dfa5ef5c35817f89bc38dd44f41c4ca
jacms <jalanmartinez@gmail.com>
2021-03-16 00:03:44 -0700

    implementacion y ejemplos

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
 # select C8 jQuery
 
-__under construction__
+SelectC8 es una librería que hace el uso del elemento select más sencillo.
+
+>*La idea base de este componente es llenar de manera dinámica los controles select en tu aplicación, centralizando código y haciendo tus llamadas más limpias, puedes utilizarlo >dentro de peticiones ajax, o en tu funciones con json listos para ser utilizados.*
+
+## Demo
+Si deseas ver mas a detalle el uso de esta librería, puedes visitar  [SelectC8 jQuery](https://jacms.github.io/Select-C8-jQuery/).
+
+ ``` javascript
+let $example4 = $("#example4");
+            const fruits4 = [{
+                    id: 1,
+                    name: "Apple",
+                    family: "Rosaceae"
+                },
+                {
+                    id: 2,
+                    name: "Lemon",
+                    family: "Rutaceae"
+                },
+                {
+                    id: 3,
+                    name: "Mango",
+                    family: "Anacardiaceae"
+                },
+                {
+                    id: 4,
+                    name: "Watermelon",
+                    family: "Cucurbitaceae"
+                }
+            ];
+            
+            $example4.SelectC8({
+                data: fruits4,
+                valueProperty: "id",
+                textProperty: "name",
+                dataExtended: "family",
+                onChange: onChangeC8
+            });
+
+            function onChangeC8(select) {
+                console.log(select.val(), $("option:selected", select).text(), $("option:selected", select).data("extended"));
+                alert($("option:selected", select).text());
+            }
+```
+
+## Creditos
+* Concept and development by [jacms](https://github.com/jacms) & [reneezy87](https://github.com/Reneezy87).
+* By ArtisCode
--- js/SelectC8jQuery.js
+++ js/SelectC8jQuery.js
@@ -1,10 +1,19 @@
+/**
+ * SelectC8 jQuery
+ * @Copyright: Alan Mtz & Rene Mtz
+ * https://github.com/jacms
+ * @version 1.0.0
+ * @author jacms & reneezy
+ */
 (function ($) {
     "use strict";
     $.fn.SelectC8 = function (options) {
-        var self = this;
+        let self = this;
         if ($(self).length === 0)
             return;
-        var defaults = {
+        if (!self.is("select"))
+            return;
+        let defaults = {
             data: null,
             valueProperty: "",
             textProperty: "",
@@ -13,18 +22,93 @@
             assignWithoutProperties: false,
             appendDefaultFirstOption: true,
             defaultFirstOptionValue: "",
-            defaultFirstOptionText: "Seleccione...",
+            defaultFirstOptionText: "-- Select option --",
             onChange: null,
-            oneChange: null,
             onSuccess: function () {
                 return false
             }
         };
+        let settings = $.extend(defaults, options);
 
-        var settings = $.extend(defaults, options);
+        function clearControl() {
+            self.empty();
+        }
 
-        console.log("object");
+        function appendDefaultOption() {
+            if (settings.appendDefaultFirstOption === true) {
+                self.append($("<option>", {
+                    value: settings.defaultFirstOptionValue,
+                    text: settings.defaultFirstOptionText
+                }));
+            }
+        }
 
+        function getPropertyValues() {
+            let values = [];
+            settings.data.forEach(object => {
+                let optionValue;
+                if (settings.assignWithoutProperties === false) {
+                    optionValue = {
+                        value: object[settings.valueProperty],
+                        text: object[settings.textProperty]
+                    };
+                } else {
+                    optionValue = {
+                        value: object,
+                        text: object
+                    };
+                }
+                if (settings.dataExtended && object[settings.dataExtended] !== undefined) {
+                    optionValue[settings.dataExtended] = object[settings.dataExtended];
+                }
+                values.push(optionValue);
+            });
+            return values;
+        }
 
+        function selectedValue() {
+            if (settings.selectedValue) {
+                self.val(settings.selectedValue);
+                $("option", self).filter(function () {
+                    return this.value === settings.selectedValue;
+                }).prop("selected", true);
+            }
+        }
+
+        function appendOptions() {
+            if (!settings.data && !Array.isArray(settings.data))
+                return;
+            let values = getPropertyValues();
+            values.forEach(object => {
+                var $option = $("<option>", object);
+                if (settings.dataExtended && object[settings.dataExtended] !== undefined) {
+                    $option.data("extended", object[settings.dataExtended]);
+                }
+                self.append($option);
+            });
+        }
+
+        function setOnChange() {
+            if (typeof settings.onChange !== "function")
+                return;
+            self.unbind("change.customSelect");
+            self.on("change.customSelect", function () {
+                settings.onChange(self);
+            });
+        }
+        let buildSelect = function () {
+            clearControl();
+            appendDefaultOption();
+            appendOptions();
+            selectedValue();
+            setOnChange();
+        }
+        let successfullResponse = function () {
+            buildSelect();
+            settings.onSuccess(settings.data);
+        }
+        if (settings.data)
+            successfullResponse();
+        return undefined;
     }
 }(jQuery));
